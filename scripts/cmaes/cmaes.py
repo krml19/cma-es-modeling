9ee81446329140e0de893298417ec264d78fccb5
Marcin Karmelita <karmelita.marcin.mk@gmail.com>
2018-04-11 20:05:35 +0200

    Refactorized objective_function

--- scripts/cmaes/cmaes.py
+++ scripts/cmaes/cmaes.py
@@ -6,18 +6,18 @@ import itertools
 
 from scripts.drawer import draw
 from scripts.utils.logger import Logger
-from scripts.utils import sampler
 
 log = Logger()
 
 
 class CMAESAlgorithm:
 
-    def __init__(self, train_X: np.matrix, valid_X: np.matrix, w0=list([1, -1]), n_constraints=2, sigma0=5,
-                 scaler=StandardScaler(), model_type='cube'):
+    def __init__(self, train_X: np.matrix, valid_X: np.matrix, w0, n_constraints: int, sigma0: float,
+                 scaler: StandardScaler, model_type: str):
         self.w0 = w0
         self.train_X = train_X
         self.valid_X = valid_X
+        self.dimensions = train_X.shape[1]
         self.n_constraints = n_constraints
         self.sigma0 = sigma0
         self.es = None
@@ -29,45 +29,30 @@ class CMAESAlgorithm:
             self.valid_X = self.scaler.transform(valid_X)
 
     def objective_function(self, w):
-        # self.draw_results(w)
-        w = np.split(w, self.n_constraints)
+        w = np.reshape(w, newshape=(self.n_constraints, -1)).T
 
-        b_final = np.ones((self.train_X.shape[0],))
-        p_final = np.ones((self.valid_X.shape[0],))
-
-        for wi in w:
-            # b
-            b = self.train_X.dot(wi)
-            # p
-            p = self.valid_X.dot(wi)
-            for wi0 in self.w0:
-                # b
-                bi = b <= wi0
-                b_final = np.multiply(bi, b_final)
-                log.debug("b: {}".format(bi))
-
-                # p
-                pi = p <= wi0
-                p_final = np.multiply(pi, p_final)
-                log.debug("p: {}".format(pi))
+        def obls(X):
+            x = np.matmul(X, w)
+            x = x <= self.w0
+            _len = x.shape[0]
+            _sum = x.prod(axis=1).sum()
+            return _len, _sum
 
         # recall
-        card_b = len(b_final)
-        tp = b_final.sum()
+        card_b, tp = obls(self.train_X)
         recall = tp / card_b
 
         # p
-        card_p = len(p_final)
-        p_final = p_final.sum()
+        card_p, p = obls(self.valid_X)
 
         # p_y
-        pr_y = p_final / card_p
+        pr_y = p / card_p
         pr_y = max(pr_y, 1e-6)  # avoid division by 0
 
         # f
-        f = (recall ** 2) / (pr_y)
+        f = (recall ** 2) / pr_y
         f = -f
-        log.info("tp: {},\trecall: {},\tp: {},\t pr_y: {},\t\tf: {}".format(tp, p_final, recall, pr_y, f))
+        log.info("tp: {},\trecall: {},\tp: {},\t pr_y: {},\t\tf: {}".format(tp, p, recall, pr_y, f))
         return f
 
     @staticmethod
@@ -82,15 +67,15 @@ class CMAESAlgorithm:
 
     @staticmethod
     def cartesian(n=1, decimals=5, r=1):
-        phis = np.arange(n)/n*2*np.pi
+        phis = np.arange(n) / n * 2 * np.pi
 
         coordinates = [CMAESAlgorithm.ct([phi], r=r, decimals=decimals) for phi in phis]
         flatten = np.array(list(itertools.chain.from_iterable(coordinates)))
         return flatten
 
     @staticmethod
-    def scale_factor(train_data_set: np.array):
-        return np.abs(train_data_set).max()
+    def scale_factor(train_data_set: np.array, offset_coefficient=2):
+        return np.abs(train_data_set).max() * offset_coefficient
 
     @staticmethod
     def bounding_sphere(n: int, train_data_set: np.array, r=1, decimals=5):
@@ -104,7 +89,7 @@ class CMAESAlgorithm:
         self.draw_results(np.array(x0), title='Initial solution: {}'.format(f))
 
         seed = 77665
-        es = cma.CMAEvolutionStrategy(x0=x0, sigma0=self.sigma0, inopts={'seed': seed, 'maxiter': int(1e7)})
+        es = cma.CMAEvolutionStrategy(x0=x0, sigma0=self.sigma0, inopts={'seed': seed, 'maxiter': int(1e6)})
 
         # iterate until termination
         while not es.stop():
@@ -128,9 +113,9 @@ class CMAESAlgorithm:
             for wi in w:
                 # p
                 p = dataset.dot(wi)
-                for wi0 in self.w0:
-                    pi = p <= wi0
-                    p_final = np.multiply(pi, p_final)
+                pi = p <= self.w0
+                p_final = np.multiply(pi, p_final)
+
             return pd.Series(data=p_final, name='valid')
 
         names = ['x_{}'.format(x) for x in np.arange(self.valid_X.shape[1])]
@@ -147,21 +132,25 @@ class CMAESAlgorithm:
 
 def data_sets(model_type: str):
     # load train data
+    # fixme Remove:
+    # train_X = pd.read_csv('data/mock/mock_valid.csv', nrows=500)
     train_X = pd.read_csv('data/train/{}2_0.csv'.format(model_type), nrows=500)
-    train_X = train_X.values
+    # train_X = np.matrix(train_X.values)
+    train_X = (train_X.values)
 
     # load valid data
     valid_X = pd.read_csv('data/validation/{}2_0.csv'.format(model_type), nrows=None)
-    valid_X = valid_X.values
+    # valid_X = np.matrix(valid_X.values)
+    valid_X = (valid_X.values)
 
     return train_X, valid_X
 
 
 # load data
-model_type = 'cube'
+model_type = 'simplex'
 train_X, valid_X = data_sets(model_type)
 
 # # run algorithm
-algorithm = CMAESAlgorithm(train_X=train_X, valid_X=valid_X, n_constraints=6, w0=[1], sigma0=1, model_type=model_type)
+algorithm = CMAESAlgorithm(train_X=train_X, valid_X=valid_X, n_constraints=3, w0=1, sigma0=1, model_type=model_type,
+                           scaler=None)
 algorithm.cma_es()
-
