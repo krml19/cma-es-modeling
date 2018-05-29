bfbe57e2d1746b7faf31e02135f9ffb34fafb72a
Marcin Karmelita <karmelita.marcin.mk@gmail.com>
2018-05-30 01:10:16 +0200

    Updated experiments

--- runner.py
+++ runner.py
@@ -176,7 +176,7 @@ class AlgorithmRunner:
 
 if __name__ == '__main__':
     runner = AlgorithmRunner()
-    experiments = flat([runner.experiments_1(), runner.experiments_2(), runner.experiments_3(), runner.experiments_4(), runner.experiments_5()])
-    # experiments = runner.experiments_1()
+    # experiments = flat([runner.experiments_1(), runner.experiments_2(), runner.experiments_3(), runner.experiments_4(), runner.experiments_5()])
+    experiments = runner.experiments_2()
     # runner.run(experiments)
     runner.run_slurm(experiments)
