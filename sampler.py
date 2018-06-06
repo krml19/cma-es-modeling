e898fbd721799f985746987ceb709360ed96acc6
Marcin Karmelita <karmelita.marcin.mk@gmail.com>
2018-06-06 02:43:57 +0200

    Fixed defining initial solution

--- sampler.py
+++ sampler.py
@@ -19,16 +19,16 @@ def ct(arr, r):
     return np.round(si * co * r, decimals=5)
 
 
-def min_pow(x: int, value: int):
-    p = x ** x
-    return x if p >= value else min_pow(x=x+1, value=value)
+def min_pow(n, x: int, value: int):
+    p = np.power(x, n-1)
+    return x if p > value else min_pow(n=n, x=x+1, value=value)
 
 
 def cartesian(n: int, dim: int, r: float=1):
     assert dim >= 2
     assert n >= dim
 
-    _n = min_pow(2, n)
+    _n = min_pow(dim, 2, n)
     _n = _n if dim > 2 else _n * 2
     phis = np.arange(_n) / _n * 2 * np.pi
 
@@ -37,7 +37,7 @@ def cartesian(n: int, dim: int, r: float=1):
     phis = itertools.islice(phis, 0, int(n))
 
     coordinates = [ct(phi, r=r) for phi in phis]
-    return np.concatenate(coordinates)
+    return coordinates
 
 
 def scale_factor(train_data_set: np.array, margin: float):
@@ -46,7 +46,6 @@ def scale_factor(train_data_set: np.array, margin: float):
 
 def bounding_sphere(n: int, train_data_set: np.array, dim: int, r=1, margin: float=2.0):
     x0 = cartesian(n, r=r, dim=dim)
-    dividers = np.tile(scale_factor(train_data_set=train_data_set, margin=margin), n)
-    x0 = x0 / dividers
-    return x0
+    x0 = x0 / scale_factor(train_data_set=train_data_set, margin=margin)
+    return np.concatenate(x0).flatten()
 
