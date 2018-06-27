b5231db3a5490508f3da7cb5bcd954cecd066872
Marcin Karmelita <karmelita.marcin.mk@gmail.com>
2018-06-27 21:01:08 +0200

    [Figures] Change PNG figures to tikzpicture

--- constraints_generator.py
+++ constraints_generator.py
@@ -1,19 +1,19 @@
 import numpy as np
 
 
-def f_2n(n: int) -> int:
+def f_2n(n):
     return 2 * n
 
-def f_2np2(n: int) -> int:
+def f_2np2(n):
     return 2 * n ** 2
 
 
-def f_n3(n: int) -> int:
-    return int(np.power(n, 3))
+def f_n3(n):
+    return np.power(n, 3)
 
 
-def f_2pn(n: int) -> int:
-    return int(np.power(2, n))
+def f_2pn(n):
+    return np.power(2, n)
 
 
 def generate(f_name, value):
@@ -33,18 +33,25 @@ def to_latex(f_name):
 def draw():
     import matplotlib.pyplot as plt
     import pandas as pd
+    from matplotlib2tikz import save as tikz_save
     functions = [f_2n, f_2np2, f_2pn, f_n3]
 
-    x = np.arange(2, 12)
-
+    xmin = 2
+    xmax = 7
+    x = np.arange(xmin, xmax, 0.1)
+    ticks = np.arange(xmin, xmax)
     items = dict()
     for f in functions:
         items[to_latex(f.__name__)] = list(map(lambda xi: f(xi), x))
 
     df = pd.DataFrame.from_dict(items)
     df.set_index(x, inplace=True)
-    df.plot(xticks=x, xlim=(2, x.max()))
-    plt.show()
+    ax = df.plot(xticks=ticks, xlim=(2, x.max()), logy=True)
+
+    ax.set_xlabel('$n$')
+    ax.set_ylabel('$n_c$')
+    # plt.show()
+    tikz_save("cg-%d-%d.tex" % (xmin, xmax))
     return df
 
 
--- probabilities.py
+++ probabilities.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from matplotlib2tikz import save as tikz_save
+from matplotlib2tikz import get_tikz_code
 
 
 class Probabilities:
@@ -19,9 +21,27 @@ class Probabilities:
         ax.set_xlabel('$n$')
         ax.set_ylabel('$V$')
 
-        plt.show()
+        tikz_save("pryn.tex")
+        # print(get_tikz_code())
+        # plt.show()
 
 
 p = Probabilities()
 p.transform()
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# plt.style.use("ggplot")
+#
+# t = np.arange(0.0, 2.0, 0.1)
+# s = np.sin(2 * np.pi * t)
+# s2 = np.cos(2 * np.pi * t)
+# plt.plot(t, s, "o-", lw=4.1)
+# plt.plot(t, s2, "o-", lw=4.1)
+# plt.xlabel("time (s)")
+# plt.ylabel("Voltage (mV)")
+# plt.title("Simple plot $\\frac{\\alpha}{2}$")
+# plt.grid(True)
+#
 
