b98cbe3d82f124c277ab823d5e61a931378cb059
Tomasz <tpawlak@cs.put.poznan.pl>
2019-06-12 17:16:21 +0200

    extra functions for calculating number of constraints modified n3 function into 1/2 n3

--- constraints_generator.py
+++ constraints_generator.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+def f_n1(n):
+    return n + 1
+
 
 def f_2n(n):
     return 2 * n
@@ -10,7 +13,7 @@ def f_2np2(n):
 
 
 def f_n3(n):
-    return np.power(n, 3)
+    return int(np.round(0.5 * np.power(n, 3)))
 
 
 def f_2pn(n):
@@ -18,15 +21,17 @@ def f_2pn(n):
 
 
 def generate(f_name, value):
-    return {f_2n.__name__: f_2n(value),
+    return {f_n1.__name__: f_n1(value),
+            f_2n.__name__: f_2n(value),
             f_2np2.__name__: f_2np2(value),
             f_n3.__name__: f_n3(value),
             f_2pn.__name__: f_2pn(value)}[f_name]
 
 
 def to_latex(f_name):
-    return {f_2n.__name__: r'$2n$',
+    return {f_n1.__name__: r'$n+1$',
+            f_2n.__name__: r'$2n$',
             f_2np2.__name__: r'$2n^2$',
-            f_n3.__name__: r'$n^3$',
+            f_n3.__name__: r'$\frac{1}{2}n^3$',
             f_2pn.__name__: r'$2^n$'}[f_name]
 
